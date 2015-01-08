4a380a4555ef69f011c31d06687ac50ec359d827
eloy <eloy.colell@gmail.com>
2015-01-08 13:12:51 -0300

    If fixed the test_sensor_calibrations test unit (issue #3).

--- stations/tests/units/__init__.py
+++ stations/tests/units/__init__.py
@@ -5,6 +5,6 @@ from test_measurements import *
 from test_optic_filters import *
 from test_positions import *
 from test_products import *
-#from test_sensor_calibrations import *
+from test_sensor_calibrations import *
 from test_sensors import *
 from test_stations import *
--- stations/tests/units/test_sensor_calibrations.py
+++ stations/tests/units/test_sensor_calibrations.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 from stations.models import *
 from django.test import TestCase
 from datetime import datetime
@@ -7,9 +7,9 @@ import pytz
 
 class TestSensorCalibrations(TestCase):
 	fixtures = [ 'initial_data.yaml', '*']
-	
+
 	def setUp(self):
-		self.calibration = SensorCalibration.objects.filter(pk = 0)[0]
+		self.calibration = SensorCalibration.objects.filter(pk = 1)[0]
 
 	def test_serialization(self):
 		# check if the __str__ method is defined to return the object name.
